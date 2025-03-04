a45564a0c321d5a9cc00c4438c6448db4cc4b6da
Israel Senior <israsenior.dev@gmail.com>
2025-03-03 21:12:05 -0500

    ssr true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,4 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   ssr: true,
-  nitro: {
-    preset: 'node-server' // Configuración de Nitro para el servidor
-  },
 })
