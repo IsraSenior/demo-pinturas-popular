6d16714cc1806c63110e78857dae033ff5e46079
Israel Senior <israsenior.dev@gmail.com>
2025-03-03 22:29:18 -0500

    ____tets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   ssr: true,
-  nitro: {
-    preset: "vercel", // Use Vercel preset for deployment
-  },
+  // nitro: {
+  //   preset: "vercel", // Use Vercel preset for deployment
+  // },
 });
--- server/api/ficha-tecnica.post.ts
+++ server/api/ficha-tecnica.post.ts
@@ -7,7 +7,9 @@ export default defineEventHandler(async (event) => {
 
   console.log("body", body);
 
-  const url = "https://demo-pinturas-popular.vercel.app/";
+  const productionURL = "https://demo-pinturas-popular.vercel.app/";
+  const devURL = "http://localhost:3000/";
+  const directusURL = "https://admin.niguafreezone.com/"
 
   // // Lanza el navegador
   const browser = await puppeteer.launch({
@@ -15,15 +17,18 @@ export default defineEventHandler(async (event) => {
     ignoreDefaultArgs: ["--disable-extensions"],
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
+
   // Abre una nueva página
   const page = await browser.newPage();
 
   // Establece el tamaño de la ventana.
   await page.setViewport({ width: 1280, height: 720 });
+
   // Carga tu HTML con TailwindCSS
-  await page.goto(`${url}ficha-tecnica?productID=${body.productID}`, {
+  await page.goto(`${productionURL}ficha-tecnica?productID=${body.productID}`, {
     waitUntil: "networkidle0",
   });
+
   // await page.evaluate(() => setTimeout(() => {}, 20000));
   await page.waitForSelector("#loaded-content", { timeout: 10000 });
 
@@ -39,37 +44,37 @@ export default defineEventHandler(async (event) => {
     }
   }, selectorToRemove);
 
-  // const pdfName = `ficha-tecnica.pdf`;
+  const pdfName = `ficha-tecnica.pdf`;
   // // Genera el PDF
   const pdfBuffer = await page.pdf({
-    path: `./public/fichas-tecnica/${body.productID}.pdf`, // Local store
+    // path: `./public/fichas-tecnica/${body.productID}.pdf`, // Local store
     format: "A4",
     // pageRanges: '1-8',
     printBackground: true,
   });
 
-  // // // // Cierra el navegador
-  // await browser.close();
+  // // // Cierra el navegador
+  await browser.close();
 
-  // const formData = new FormData();
+  const formData = new FormData();
 
-  // const pdfBlob = new Blob([pdfBuffer], { type: "application/pdf" });
+  const pdfBlob = new Blob([pdfBuffer], { type: "application/pdf" });
 
-  // formData.append("folder", config.private.directus.folders.orders);
-  // formData.append("title", pdfName);
-  // formData.append("file", pdfBlob);
+  formData.append("folder", "1391c2b3-3477-40d0-a3a1-ba5e0bef8208");
+  formData.append("title", pdfName);
+  formData.append("file", pdfBlob);
 
-  // const uploadFile = await fetch(`${config.private.directus.url}/files`, {
-  //   method: "POST",
-  //   headers: {
-  //     Accept: "application/json",
-  //     Authorization: `Bearer ${config.private.directus.token}`,
-  //   },
-  //   body: formData,
-  // });
+  const uploadFile = await fetch(`${directusURL}files`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer bYyxa_1Se6D3itri_G3jJ96bRhMN8rxo`,
+    },
+    body: formData,
+  });
 
-  // const { data: uploadRes } = await uploadFile.json();
-
-  // return uploadRes.id;
-  return `https://demo-pinturas-popular.vercel.app/fichas-tecnica/${body.productID}.pdf`;
+  const { data: uploadRes } = await uploadFile.json();
+  console.log(uploadRes.id);
+  return uploadRes.id;
+  // return `https://demo-pinturas-popular.vercel.app/fichas-tecnica/${body.productID}.pdf`;
 });
